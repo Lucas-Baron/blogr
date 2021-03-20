8fe866b7e9322ae0f61f0ae31fe118fbaec592a1
Lucas Baron <lucasbaron28@hotmail.com>
2021-03-20 20:27:17 -0300

    Tentando adicionar js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
 const connect = document.querySelector('#connect')
+const content = document.querySelector('.content')
+const img = connect.querySelector('img')
+
 
     connect.addEventListener('click', function(){
-        connect.querySelector('.content').classList.add('active')
+        content.classList.add('active')
+        img.classList.add('run')
     })
 
+const click = connect.querySelector('#click')
+
+    click.addEventListener('click', function(){
+        content.classList.remove('active')
+        img.classList.remove('run')
+    })
+
+
+    
