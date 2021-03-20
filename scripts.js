51b9190a28d70f4030642c4249f3a1fe5c2112f9
Lucas Baron <lucasbaron28@hotmail.com>
2021-03-20 18:00:51 -0300

    Add Submenu

--- scripts.js
+++ scripts.js
@@ -1,24 +1,6 @@
-    const modal = document.querySelector('.modal')
-    const modalMenu = document.querySelectorAll('.area-modal')
+const connect = document.querySelector('#connect')
 
-    for (let areaModal of modalMenu){
-        areaModal.addEventListener('click', function(){
+    connect.addEventListener('click', function(){
+        connect.querySelector('.content').classList.add('active')
+    })
 
-            modal.classList.add('active')
-        })
-    }
-
-
-   
-
-
-
-    /*for (let card of cards){
-        card.addEventListener("click", function(){
-            const videioId = card.getAttribute('id')
-            modalOverlay.classList.add('active')
-    
-            modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videioId}`
-        })
-    }
-    */
